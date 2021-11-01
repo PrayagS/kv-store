c5dbccff3ff39e0647151c8298258485cd48191f
PrayagS <prayag.s@ahduni.edu.in>
2021-11-01 18:58:15 +0530

    refactor: Change value datatype to string

--- pkg/kvstore/kvstore.go
+++ pkg/kvstore/kvstore.go
@@ -4,26 +4,22 @@ import (
 	"fmt"
 )
 
-type Value struct {
-	Object interface{}
-}
-
 type KVStore struct {
-	values map[string]Value
+	values map[string]string
 }
 
 func New() KVStore {
 	kvs := KVStore{
-		values: make(map[string]Value),
+		values: make(map[string]string),
 	}
 	return kvs
 }
 
-func (kvs KVStore) Set(key string, value interface{}) {
-	kvs.values[key] = Value{Object: value}
+func (kvs KVStore) Set(key string, value string) {
+	kvs.values[key] = value
 }
 
-func (kvs KVStore) Get(key string) (Value, error) {
+func (kvs KVStore) Get(key string) (string, error) {
 	v, ok := kvs.values[key]
 	if !ok {
 		return v, fmt.Errorf("the key %s does not exist", key)
