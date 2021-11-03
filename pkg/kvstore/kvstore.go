0671b46e20d94fb850afa2926ee396051b2f6d11
PrayagS <prayag.s@ahduni.edu.in>
2021-11-03 13:01:59 +0530

    refactor: values -> Pairs

--- pkg/kvstore/kvstore.go
+++ pkg/kvstore/kvstore.go
@@ -5,22 +5,22 @@ import (
 )
 
 type KVStore struct {
-	values map[string]string
+	Pairs map[string]string
 }
 
 func New() KVStore {
 	kvs := KVStore{
-		values: make(map[string]string),
+		Pairs: make(map[string]string),
 	}
 	return kvs
 }
 
 func (kvs KVStore) Set(key string, value string) {
-	kvs.values[key] = value
+	kvs.Pairs[key] = value
 }
 
 func (kvs KVStore) Get(key string) (string, error) {
-	v, ok := kvs.values[key]
+	v, ok := kvs.Pairs[key]
 	if !ok {
 		return v, fmt.Errorf("the key %s does not exist", key)
 	}
